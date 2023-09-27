0d52ee8b37ea4879403e6079fd8f62082726d273
arlotetxu <arlotetxu@protonmail.com>
2023-09-27 17:01:27 +0200

    ft_strspn.c understood

--- Exam_files_grademe/Review_1/ft_strspn.c
+++ Exam_files_grademe/Review_1/ft_strspn.c
@@ -6,7 +6,7 @@
 /*   By: jflorido <jflorido@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 16:49:38 by jflorido          #+#    #+#             */
-/*   Updated: 2023/09/26 16:54:56 by jflorido         ###   ########.fr       */
+/*   Updated: 2023/09/27 16:07:20 by jflorido         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,13 +26,40 @@ size_t	ft_strspn(const char *s, const char *accept);
 #include <string.h>
 #include <stdio.h>
 
+int	ft_check_char(const char *str, char c)
+{
+	int	i;
+
+	i= -1;
+	while (str[++i])
+	{
+		if (str[i] == c)
+			return (0);
+	}
+	return (1);
+}
+
+size_t	ft_strspn(const char *s, const char *accept)
+{
+	size_t	i;
+
+	i = -1;
+	while (s[++i])
+	{
+		if (ft_check_char(accept, s[i]) == 1)
+			return (i);
+	}
+	return (0);
+}
+
 int	main(void)
 {
 	char	*s;
 	char	*accept;
 
 	s = "abcdefghijk";
-	accept = "bcd";
-	printf("Res: %ld\n", strspn(s, accept));
+	accept = "zzabzdc";
+	printf("Res_original: %ld\n", strspn(s, accept));
+	printf("Res_created: %ld\n", ft_strspn(s, accept));
 	return (0);
 }
